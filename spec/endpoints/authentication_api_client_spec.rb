9657e7d4b63a3655e1400dd1b0b28255aa9f9fe7
Blackfaded <r.heinen@nanogiants.de>
2025-07-10 15:20:04 +0200

    test: update test

--- spec/endpoints/authentication_api_client_spec.rb
+++ spec/endpoints/authentication_api_client_spec.rb
@@ -1,17 +1,19 @@
 
+
 require 'spec_helper'
 require 'net/http'
 require 'json'
 require_relative '../../lib/PCP-server-Ruby-SDK.rb'
 
-describe PCPServerSDK::Endpoints::AuthenticationApiClient do
-  let(:config) { double('PCPServerSDK::CommunicatorConfiguration') }
-  let(:client) { described_class.new(config) }
+
+RSpec.describe PCPServerSDK::Endpoints::AuthenticationApiClient do
+  let(:config) { double('PCPServerSDK::CommunicatorConfiguration', api_key: '', api_secret: '', host: 'https://api.example.com') }
+  let(:client) { PCPServerSDK::Endpoints::AuthenticationApiClient.new(config) }
 
   describe '#get_authentication_tokens' do
     let(:merchant_id) { 'merchant123' }
     let(:request_id) { 'req-456' }
-    let(:response_body) {
+    let(:success_response_body) {
       {
         token: 'jwt-token',
         id: 'uuid-123',
@@ -19,23 +21,58 @@ describe PCPServerSDK::Endpoints::AuthenticationApiClient do
         expirationDate: '2025-07-10T12:10:00Z'
       }.to_json
     }
+    let(:error_body) {
+      PCPServerSDK::Models::ErrorResponse.new(
+        error_id: '1',
+        errors: [PCPServerSDK::Models::APIError.new(error_code: '1', message: 'Error 1')]
+      ).to_body.to_json
+    }
 
-    before do
-      allow(client).to receive(:build_url).and_return('https://api.test/auth')
-      allow(client).to receive(:post).and_return(double('Response', body: response_body))
+    context 'when request is successful' do
+      let(:response) { double('Response', body: success_response_body, code: 200) }
+
+      before do
+        allow(client).to receive(:post).and_return(response)
+      end
+
+      it 'returns an AuthenticationToken' do
+        token = client.get_authentication_tokens(merchant_id, request_id)
+        expect(token).to be_a(PCPServerSDK::Models::AuthenticationToken)
+        expect(token.token).to eq('jwt-token')
+        expect(token.id).to eq('uuid-123')
+        expect(token.creation_date).to be_a(Time)
+        expect(token.expiration_date).to be_a(Time)
+      end
     end
 
-    it 'returns an AuthenticationToken' do
-      token = client.get_authentication_tokens(merchant_id, request_id)
-      expect(token).to be_a(PCPServerSDK::Models::AuthenticationToken)
-      expect(token.token).to eq('jwt-token')
-      expect(token.id).to eq('uuid-123')
-      expect(token.creation_date).to be_a(Time)
-      expect(token.expiration_date).to be_a(Time)
+    context 'when request is unsuccessful (400)' do
+      let(:response) { double('Response', body: error_body, code: 400) }
+
+      before do
+        allow(client).to receive(:post).and_return(response)
+      end
+
+      it 'raises an PCPServerSDK::Errors::ApiErrorResponseException' do
+        expect { client.get_authentication_tokens(merchant_id, request_id) }.to raise_error(PCPServerSDK::Errors::ApiErrorResponseException)
+      end
     end
 
-    it 'raises error if merchant_id is nil' do
-      expect { client.get_authentication_tokens(nil) }.to raise_error(ArgumentError)
+    context 'when request is unsuccessful (500)' do
+      let(:response) { double('Response', body: '{}', code: 500) }
+
+      before do
+        allow(client).to receive(:post).and_return(response)
+      end
+
+      it 'raises an PCPServerSDK::Errors::ApiResponseRetrievalException' do
+        expect { client.get_authentication_tokens(merchant_id, request_id) }.to raise_error(PCPServerSDK::Errors::ApiResponseRetrievalException)
+      end
+    end
+
+    context 'when merchant_id is nil' do
+      it 'raises an ArgumentError' do
+        expect { client.get_authentication_tokens(nil) }.to raise_error(ArgumentError)
+      end
     end
   end
 end
