55d757f9427b16513b6c48bd9a8d7332d82bd886
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2026-05-21 10:37:49 +0200

    feat: update API version to 1.55.0

--- spec/endpoints/payment_execution_api_client_spec.rb
+++ spec/endpoints/payment_execution_api_client_spec.rb
@@ -377,4 +377,56 @@ RSpec.describe PCPServerSDK::Endpoints::PaymentExecutionApiClient do
       end
     end
   end
+
+  describe '#create_fund_split' do
+    let(:payload) { double('PCPServerSDK::Models::FundSplitRequest') }
+
+    context 'when request is successful' do
+      let(:response) { double('Response', body: '{}', code: '200') }
+      let(:expected_response) { PCPServerSDK::Models::FundSplitResponse.new }
+
+      before do
+        allow(client).to receive(:get_response).and_return(response)
+      end
+
+      it 'returns a successful response' do
+        result = client.create_fund_split('1', '2', '3', '4', '5', payload)
+        expect(result).to eq(expected_response)
+      end
+    end
+
+    context 'when request is unsuccessful (400)' do
+      let(:response) { double('Response', body: error_body, code: '400') }
+
+      before do
+        allow(client).to receive(:get_response).and_return(response)
+      end
+
+      it 'raises an PCPServerSDK::Errors::ApiErrorResponseException' do
+        expect { client.create_fund_split('1', '2', '3', '4', '5', payload) }.to raise_error(PCPServerSDK::Errors::ApiErrorResponseException)
+      end
+    end
+
+    context 'when request is unsuccessful (500)' do
+      let(:response) { double('Response', body: '{}', code: '500') }
+
+      before do
+        allow(client).to receive(:get_response).and_return(response)
+      end
+
+      it 'raises an PCPServerSDK::Errors::ApiResponseRetrievalException' do
+        expect { client.create_fund_split('1', '2', '3', '4', '5', payload) }.to raise_error(PCPServerSDK::Errors::ApiResponseRetrievalException)
+      end
+    end
+
+    context 'when some params are nil' do
+      it 'raises a TypeError' do
+        expect { client.create_fund_split(nil, '2', '3', '4', '5', payload) }.to raise_error(TypeError, 'Merchant ID is required')
+        expect { client.create_fund_split('1', nil, '3', '4', '5', payload) }.to raise_error(TypeError, 'Commerce Case ID is required')
+        expect { client.create_fund_split('1', '2', nil, '4', '5', payload) }.to raise_error(TypeError, 'Checkout ID is required')
+        expect { client.create_fund_split('1', '2', '3', nil, '5', payload) }.to raise_error(TypeError, 'Payment Execution ID is required')
+        expect { client.create_fund_split('1', '2', '3', '4', nil, payload) }.to raise_error(TypeError, 'Event ID is required')
+      end
+    end
+  end
 end
