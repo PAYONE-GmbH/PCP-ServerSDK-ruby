9b5ebe858a46e671c57c85902445543dbbddcec2
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2026-07-22 07:32:27 +0200

    feat: update API version to 1.60.0

--- lib/PCP-server-Ruby-SDK/models/redirect_payment_product840_specific_input.rb
+++ lib/PCP-server-Ruby-SDK/models/redirect_payment_product840_specific_input.rb
@@ -4,6 +4,7 @@ module PCPServerSDK
   module Models
     class RedirectPaymentProduct840SpecificInput < RedirectPaymentProduct840SpecificInputData
       attribute :fraud_net_id, :fraudNetId, :String
+      attribute :payment_id, :paymentId, :String
 
       def self.openapi_all_of
         [:RedirectPaymentProduct840SpecificInputData]
